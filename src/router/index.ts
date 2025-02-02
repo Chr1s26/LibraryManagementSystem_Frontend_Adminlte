990d5d696a1488978ac61259368acdbc9dd3f84d
Chr1s26 <htetkyawswarlinn44@gmail.com>
2025-02-02 21:06:57 +0700

    uploaded

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MainLayout from '../layouts/MainLayout.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path:'/',
+    component: MainLayout,
+    children: [
+      {
+        path:'/',
+        name: 'home',
+        component:HomeView
+      },{
+        path: '/about',
+        name: 'about',
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    app:{
+      name: 'LMS Admin'
+    }
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
+
+
